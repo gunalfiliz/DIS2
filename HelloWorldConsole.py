de2db5bc8fd8d7a850d9745c137709fe6a5ba03b
Filiz Günal <47874164+gunalfiliz@users.noreply.github.com>
2023-12-19 23:13:33 +0100

    Add files via upload

---
+++ HelloWorldConsole.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""
+Submission for Project Milestone 1, Task 2
+by  Student Name 1 (#999999)
+and Student Name 2 (#999999)
+"""
+
+#insert your code here
---
+++ HelloWorldGraphical.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""
+Submission for Project Milestone 1, Task 3
+by  Student Name 1 (#999999)
+and Student Name 2 (#999999)
+"""
+
+from tkinter import *
+
+window = Tk()
+window.geometry('250x300')
+
+languageLabel = Label(window, text='Please select a language.', fg = '#F6A800')
+languageLabel.pack(pady=4)
+
+#insert your code here
+
+window.mainloop()
